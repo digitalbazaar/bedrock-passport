eb8a4a5967a3ae9153fb3340aaa9ce7c4e5f35dd
Dave Longley <dlongley@digitalbazaar.com>
2022-04-06 00:44:16 -0400

    Convert to module (ESM).

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
-/*
- * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
+/*!
+ * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
  */
-const bedrock = require('bedrock');
-require('bedrock-passport');
+import * as bedrock from '@bedrock/core';
+import '@bedrock/passport';
+import '@bedrock/test';
 
-require('bedrock-test');
 bedrock.start();
